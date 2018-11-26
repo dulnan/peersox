0a810a071e1beb66215787890048d83841c807b4
Jan Hug <me@dulnan.net>
2018-11-26 16:15:22 +0100

    Docs for server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,58 @@
-/** @module server */
+/**
+ * The server manages the generation and validation of pairing codes and hashes.
+ * They are stored in redis, for local development redis-mock is used.
+ *
+ * Generated Pairings are only valid for a short amount of time. If they are not
+ * validated in that time slot, they are deleted. If they are validated, the
+ * code is removed from redis, while the hash's expire time is increased.
+ *
+ * ### Minimal example
+ * This will start a server on port 3000, using redis-mock for storage.
+ * It is not meant for production use.
+ *
+ * ```javascript
+ * var PeerSoxServer = require('peersox/lib/peersox.server.js').default
+ * new PeerSoxServer()
+ * ```
+ *
+ * ### Advanced example
+ * Connect and use a redis server for storage and provide an express middleware
+ * that prevents brute force attacks on the server.
+ *
+ * ```javascript
+ * const PeerSoxServer = require('peersox/lib/peersox.server.js').default
+ * const ExpressBrute = require('express-brute')
+ * const redis = require('redis')
+ * const BruteRedis = require('express-brute-redis')
+ *
+ * // Create a new redis client.
+ * const redisClient = redis.createClient('//localhost:6379')
+ *
+ * // Init the PeerSox server when the redis client is ready.
+ * redisClient.on('ready', function () {
+ *   console.log('Connected to Redis at')
+ *
+ *   // Use the redis client as the store for express-brute.
+ *   const bruteStore = new BruteRedis({
+ *     client: redisClient
+ *   })
+ *
+ *   // Instantiate the express-brute middleware.
+ *   const bruteforce = new ExpressBrute(bruteStore, {
+ *     freeRetries: 20
+ *   })
+ *
+ *   // Instantiate the PeerSox server.
+ *   new PeerSoxServer({
+ *     storage: redisClient,
+ *     middleware: [
+ *       bruteforce.prevent
+ *     ]
+ *   })
+ * })
+ * ```
+ *
+ * @module server
+ */
 
 export { PeerSoxServer as default } from './server/PeerSoxServer'
-export { getRedisClient } from './server/utils/storage'
--- src/server/PeerSoxServer.js
+++ src/server/PeerSoxServer.js
@@ -1,18 +1,12 @@
 import express from 'express'
 import http from 'http'
+import redisMock from 'redis-mock'
 import Store from './Store.js'
 import API from './API.js'
 import Socket from './Socket.js'
 
 /**
  * The PeerSox server class.
- *
- * The server manages the generation and validation of pairing codes and hashes.
- * They are stored in redis, for local development redis-mock is used.
- *
- * Generated Pairings are only valid for a short amount of time. If they are not
- * validated in that time slot, they are deleted. If they are validated, the
- * code is removed from redis, while the hash's expire time is increased.
  *
  * @class
  * @module server
@@ -22,18 +16,18 @@ export class PeerSoxServer {
    * @param {object} options
    * @param {object} options.app An existing express app.
    * @param {object} options.server An existing http server.
-   * @param {object} options.storage The storage client to use for the store.
+   * @param {object} options.redisClient The redis client to use for the store.
    * @param {number} options.port The port on which the server should run.
    * @param {array} options.middleware Additional middleware for use in express.
    */
   constructor ({
     app = express(),
     server,
-    storage,
+    redisClient = redisMock.createClient(),
     port = process.env.PORT || 3000,
     middleware = []
   } = {}) {
-    this.store = new Store(storage)
+    this.store = new Store(redisClient)
     this.server = server || http.createServer(app)
 
     this.api = new API({
